d29c78ae9dd43bd7d0f2d81f0dfea1061686816b
John-Akech <j.akech@alustudent.com>
2023-09-09 12:05:17 -0700

    Regular expression

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-#Regular expression
-#puts ARGV[0].scan(/School/).join
+# Regular expression must matches school, this script is written by AROMA
+puts ARGV[0].scan(/School/).join
