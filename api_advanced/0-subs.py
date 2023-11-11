9e9161a3324ebbaa142fbc01508ebdaa22d578ca
John-Akech <j.akech@alustudent.com>
2023-11-11 09:51:18 +0000

    API ADVANCED

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,8 +4,8 @@ A function that queries the Reddit API and returns the number of subscribers
 """
 import sys
 
- def number_of_subscribers(subreddit):
-    """ number_of_subscribers """
+def number_of_subscribers(subreddit):
+""" number_of_subscribers """
     if __name__ == '__main__':
         number_of_subscribers = __import__('0-subs').number_of_subscribers
         if len(sys.argv) < 2:
