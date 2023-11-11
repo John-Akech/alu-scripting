1b4989b10bac209d91d640b00960b6e09b0658bc
John-Akech <j.akech@alustudent.com>
2023-11-11 09:43:15 +0000

    API advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,8 +6,8 @@ A function that queries the Reddit API and returns the number of subscribers
 import sys
 
 def number_of_subscribers(subreddit):
-    """ number_of_subscribers """
-    if __name__ == '__main__':
+""" number_of_subscribers """
+if __name__ == '__main__':
             number_of_subscribers = __import__('0-subs').number_of_subscribers
             if len(sys.argv) < 2:
                 print("Please pass an argument for the subreddit to search.")
